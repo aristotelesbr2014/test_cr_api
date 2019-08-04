67d9e8a7b66c52b6f9842a736892416cb3bed461
Aristoteles <aristotelesbr2014@gmail.com>
2019-08-04 18:23:27 -0300

    added migrate to update default value to quantity from cart_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_213811) do
+ActiveRecord::Schema.define(version: 2019_08_04_205112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_213811) do
   create_table "cart_items", force: :cascade do |t|
     t.bigint "cart_id"
     t.bigint "medicine_id"
-    t.integer "quantity"
+    t.integer "quantity", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
