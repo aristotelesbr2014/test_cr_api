901b78e454f9d428e30e0a317d6ea4b91ab29286
Aristoteles <aristotelesbr2014@gmail.com>
2019-08-03 21:12:26 -0300

    Return titleize from Customer name attribute

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   has_many :carts
+
+  def name=(str)
+    super str.try(:titleize)
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Customer, type: :model do
   context 'relationships' do
     it { is_expected.to have_many :carts }
   end
+
+  describe '.name' do
+    context 'When retuns valid data' do
+      let(:valid_attributes) { { name: 'jhon Doe' } }
+      subject { Customer.new(valid_attributes) }
+
+      it 'Returns name with first capital letter' do
+        expect(subject.name).to eql('Jhon Doe')
+      end
+    end
+  end
 end
