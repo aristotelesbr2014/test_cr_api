a6cf0c511a5bea11de06cc791455d2e0d6ded59b
Aristoteles <aristotelesbr2014@gmail.com>
2019-08-05 00:34:59 -0300

    Added sum all items_cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ApplicationRecord
   belongs_to :customer
   has_many :cart_items
+
+  def total
+    cart_items.reduce(0) { |sum, item| sum + (item.quantity * item.medicine.value) }
+  end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+  let(:custumer) { Customer.create(name: 'jhon Doe') }
+  let(:medicine) { Medicine.create(name: 'Aspirina', value: 10.0, quantity: 1, stock: 10) }
+  let(:cart) { Cart.create(customer_id: custumer.id) }
+
   context 'relationships' do
     it { is_expected.to belong_to :customer }
+  end
+
+  describe '#total' do
+    subject { Carts::AddItem.perform(medicine, cart, 10) }
+    context 'Total itens' do
+      it 'returns total of cart' do
+        expect(subject.send(:cart).total).to eql(100.0)
+      end
+    end
   end
 end
